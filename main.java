29db86064680fc6038a58faab8ba24f34cbbc82b
moezwalha20-arch <moezwalha20@gmail.com>
2025-08-08 17:11:49 +0100

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String args[]) {
         int x = 5
         int y = 10;
-        
         System.out.println("The sum is: " + x + y);
         
         String message = "Hello World
