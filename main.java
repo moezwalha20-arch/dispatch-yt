9240d6744245c5c3ed73597e9dffc0b1ab21c44f
moezwalha20-arch <moezwalha20@gmail.com>
2025-08-08 11:22:21 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String args[]) {
+        int x = 5
+        int y = 10;
+        
+        System.out.println("The sum is: " + x + y);
+        
+        String message = "Hello World
+        System.out.println(message);
+        
+        if (x = y) {
+            System.out.println("x and y are equal");
+        }
+    }
+}
